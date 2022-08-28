801434b0858b1bbb0e352110f4dc019e11b183d7
dhambure98 <akila789liyanage@gmail.com>
2022-08-28 14:05:22 +0530

    Update all get post in post publish user id router to post.js file

--- routes/post.js
+++ routes/post.js
@@ -2,71 +2,86 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 
-const Post = require('../models/post.model')
-
+const Posts = require('../model/post.model')
 
 app.use(express.json())
 
-router.get('/', async (req, res) => {
+// get all post
+router.get('/',async (req, res) =>{
     try {
-        const posts = await Post.find()
+        const posts = await Posts.find()
         res.json(posts)
-    } catch (err) {
-        res.send('Err: ' + err)
+    }catch (error) {
+        res.send('Error : '+ error)
     }
 })
 
-router.get('/:id', async (req, res) => {
+// get post in post id
+router.get('/:id',async (req, res) =>{
     try {
-        const posts = await Post.findById(req.params.id)
-        res.json(posts)
-    } catch (err) {
-        res.send('Err: ' + err)
+        const post = await Posts.findById(req.params.id)
+        res.json(post)
+    }catch (error) {
+        res.send('Error : '+ error)
     }
 })
 
-router.post('/', async (req, res) => {
-    const posts = new Post({
-        userId: req.body.userId,
-        date: req.body.date,
-        time: req.body.time,
-        title: req.body.title,
-        body: req.body.body
+// get post in post publish user id
+router.get('/:userId',async (req, res) =>{
+    try {
+        const posts = await Posts.find({ userId: req.params.userId })
+
+        res.json(posts)
+    }catch (error) {
+        res.send('Error : '+ error)
+    }
+})
+
+// save post
+router.post('/',async (req,res) => {
+    const posts = new Posts({
+        userId:req.body.userId,
+        date:req.body.date,
+        time:req.body.time,
+        title:req.body.title,
+        body:req.body.body
     })
 
     try {
-        const response = await posts.save()
+        const post = await posts.save()
+        res.json(post)
+    }catch (error) {
+        res.send('Error : '+ error)
+    }
+
+})
+
+// update post for search post id
+router.put('/:id',async (req,res) =>{
+    try {
+        const post = await Posts.findById(req.params.id)
+        post.userId = req.body.userId
+        post.date = req.body.date
+        post.time = req.body.time
+        post.title = req.body.title
+        post.body = req.body.body
+
+        const response = await post.save()
+
         res.json(response)
-    } catch (err) {
-        res.send('Err: ' + err)
+    }catch (error) {
+        res.send('Error : '+error)
     }
 })
 
-router.delete('/:id', async (req, res) => {
+// delete post
+router.delete('/:id',async (req,res) =>{
     try {
-        const posts = await Post.findById(req.params.id)
-        const response = await posts.remove()
-
+        const post = await Posts.findById(req.params.id)
+        const response = await post.remove()
         res.json(response)
-    } catch (err) {
-        res.send('Err: ' + err)
-    }
-})
-
-router.put('/:id', async (req, res) => {
-    try {
-        const posts = await Post.findById(req.params.id)
-        posts.userId = req.body.userId
-        posts.date = req.body.date
-        posts.time = req.body.time
-        posts.title = req.body.title
-        posts.body = req.body.body
-
-        const response = await posts.save()
-        res.json(response)
-
-    } catch (err) {
-        res.send('Err: ' + err)
+    }catch (error) {
+        res.send('Error : '+error)
     }
 })
 
