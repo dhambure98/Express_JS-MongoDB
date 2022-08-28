d0b137cf4318a1c7f55d24f4756dcba9f267b02c
dhambure98 <akila789liyanage@gmail.com>
2022-08-28 14:10:09 +0530

    Update all uername/pw login route login.js Done

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 
-const Login = require('../models/login.model')
-const User = require('../models/user.model')
+const Login = require('../model/login.model')
+const User = require('../model/user.model')
 
 
 app.use(express.json())
 
+// get all login username and password
 router.post('/', async (req, res) => {
     try {
         const userCount=await User.findOne({email: new RegExp('^'+req.body.email+'$', "i")}).count();
@@ -23,18 +24,18 @@ router.post('/', async (req, res) => {
 
                 });
                 const response=await login.save()
-                res.send(user.firstName +" logged in")
-                // res.send(response)
+                res.send(user.firstName +"Successful logged in")
+            
             }else {
-                res.send("Wrong Password.Try again")
+                res.send("Wrong Password. Please Input Correct Password.Try again")
             }
 
         }else {
             res.send("User not exist please check email")
         }
 
-    }catch (err){
-        res.send('Err'+err)
+    }catch (error){
+        res.send('Error : '+ error)
     }
 })
 
